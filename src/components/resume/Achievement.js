5649a60855b90aad39092e24ca85d7733ea7cb4a
rutul <prajapatirutul1223@gmail.com>
2025-01-29 15:15:39 +0530

    achievements updated

--- src/components/resume/Achievement.js
+++ src/components/resume/Achievement.js
@@ -15,27 +15,27 @@ const Achievement = () => {
           <h2 className="text-4xl font-bold">Company Experience</h2>
         </div>
         <div className="mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
-          <ResumeCard
-            title="Lorem ipsum dolor sit amet."
-            subTitle="Lorem ipsum dolor sit amet alternative."
-            result="Success"
-            des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora itaque accusamus corporis dolor debitis natus sapiente, voluptates possimus minima totam!"
+        <ResumeCard
+            title="Full Stack Developer"
+            subTitle="iFlair Web Tech - (2024 - Present)"
+            result="Ahmedabad"
+            des="iFlair's hiring process is an important part of our culture. iFlairians care deeply about their teams and the people who make them up."
           />
+          {/* <ResumeCard
+            title="Web Developer & Trainer"
+            subTitle="Apple Developer Team - (2012 - 2016)"
+            result="MALAYSIA"
+            des="A popular destination with a growing number of highly qualified homegrown graduates, it's true that securing a role in Malaysia isn't easy."
+          /> */}
           <ResumeCard
-            title="Lorem ipsum dolor sit amet."
-            subTitle="Lorem ipsum dolor sit amet alternative."
-            result="Success"
-            des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora itaque accusamus corporis dolor debitis natus sapiente, voluptates possimus minima totam!"
-          />
-          <ResumeCard
-            title="Lorem ipsum dolor sit amet."
-            subTitle="Lorem ipsum dolor sit amet alternative."
-            result="Success"
-            des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora itaque accusamus corporis dolor debitis natus sapiente, voluptates possimus minima totam!"
+            title="Internship - (Online)"
+            subTitle="React js - (2023)"
+            result="Ahmedabad "
+            des="The Ahmedabad economy has grown strongly over recent years, having transformed itself from a producer and innovation-based economy."
           />
         </div>
       </div>
-      <div>
+      {/* <div>
         <div className="py-12 font-titleFont flex flex-col gap-4">
           <p className="text-sm text-designColor tracking-[4px]">2024 - 2025</p>
           <h2 className="text-4xl font-bold">Job Experience</h2>
@@ -60,7 +60,7 @@ const Achievement = () => {
             des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora itaque accusamus corporis dolor debitis natus sapiente, voluptates possimus minima totam!"
           />
         </div>
-      </div>
+      </div> */}
     </motion.div>
   );
 };
